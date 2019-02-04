a03ab282f4e51f633c1c14c7f2b6d68d73ccec1a
Linyxus <linyxus00@gmail.com>
2019-02-04 19:25:05 +0800

    ⚙Fix: fetching all urls is not necessary

--- cloudfunctions/Subjects/handlers/fetchSubjectPunch.js
+++ cloudfunctions/Subjects/handlers/fetchSubjectPunch.js
@@ -10,12 +10,13 @@ const handler = async (e, ctx) => {
     const db = cloud.database().collection('punch')
     const data = await db.where({subjectId: subjectId}).orderBy('createdAt', 'desc').get()
     const punch = data.data
-    const imageProms = punch.map(p => cloud.getTempFileURL(
-      {fileList: p.imageIds}
-    ))
-    const imageFiles = await Promise.all(imageProms)
-    const imageUrls = imageFiles.map(f => f.fileList.map(x => x.tempFileURL))
-    return Object.assign({}, punch, {images: imageUrls})
+    // const imageProms = punch.map(p => cloud.getTempFileURL(
+    //   {fileList: p.imageIds}
+    // ))
+    // const imageFiles = await Promise.all(imageProms)
+    // const imageUrls = imageFiles.map(f => f.fileList.map(x => x.tempFileURL))
+    // return Object.assign({}, punch, {images: imageUrls})
+    return punch
 }
 
 exports.handler = handler
